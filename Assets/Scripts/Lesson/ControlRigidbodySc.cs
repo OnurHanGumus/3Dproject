ebc567d280d0e0788eac4c7fd411c6a282fd4690
DESKTOP-JM0Q4QR\Kzljd <onurhangumus@outlook.com>
2021-07-06 10:24:37 +0300

    add: a script that controls rigidbody added also set (scriptableobject) file also added

---
+++ Assets/Scripts/Lesson/ControlRigidbodySc.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlRigidbodySc : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 jumpForce;
+    Rigidbody rig;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+    }
+
+   void Update()
+    {
+        bool spaceDown = Input.GetButtonDown("Jump") ;
+        if (spaceDown)
+        {
+            rig.AddForce(jumpForce,ForceMode.Impulse);
+        }
+
+    }
+}
