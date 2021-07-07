4c23b0f318ebde27fc89fee372fbf50a8bdd54fd
DESKTOP-JM0Q4QR\Kzljd <onurhangumus@outlook.com>
2021-07-07 07:51:13 +0300

    refactor: namespaces fixed

--- Assets/Scripts/TopDownShooter/CameraControllerSet.cs
+++ Assets/Scripts/TopDownShooter/CameraControllerSet.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace TopDownShooter
+namespace TopDownShooter.Camera
 {
 
     [CreateAssetMenu(menuName = "Lessons/Lessons1/CameraController Settings")]
