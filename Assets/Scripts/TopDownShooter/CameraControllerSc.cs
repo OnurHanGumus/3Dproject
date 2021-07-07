631ef4052b7d1a36662c4aba7621d3de1fc78d13
DESKTOP-JM0Q4QR\Kzljd <onurhangumus@outlook.com>
2021-07-07 07:28:07 +0300

    add: camera controller

---
+++ Assets/Scripts/TopDownShooter/CameraControllerSc.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter
+{
+    public class CameraControllerSc : MonoBehaviour
+    {
+
+        [SerializeField]
+        CameraControllerSet cameraControllerSet;
+        [SerializeField]
+        Transform targetTransform;
+        
+        void Update()
+        {
+            CameraRotate();
+            CameraFollow();
+        }
+
+        void CameraRotate()
+        {
+
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.LookRotation(targetTransform.position - transform.position), Time.deltaTime * cameraControllerSet.RotationSpeed);
+
+
+        }
+
+        void CameraFollow()
+        {
+
+            transform.position = Vector3.Lerp(transform.position, targetTransform.position + cameraControllerSet.Offset, Time.deltaTime * cameraControllerSet.PositionLerp);
+
+
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/TopDownShooter/CameraControllerSet.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter
+{
+
+    [CreateAssetMenu(menuName = "Lessons/Lessons1/CameraController Settings")]
+
+    public class CameraControllerSet : ScriptableObject
+    {
+        [Header("rotation")]
+
+        [SerializeField]
+        private int rotationSpeed;
+
+        public int RotationSpeed
+
+        {
+            get { return rotationSpeed; }
+           private set { rotationSpeed = value; }
+        }
+        [Header("position")]
+
+        [SerializeField]
+        private Vector3 offset;
+
+        public Vector3 Offset
+
+        {
+            get { return offset; }
+            private set { offset = value; }
+        }
+
+
+        [SerializeField]
+        private float positionLerp;
+
+        public float PositionLerp
+
+        {
+            get { return positionLerp; }
+            private set { positionLerp = value; }
+        }
+
+    }
+
+}
+
