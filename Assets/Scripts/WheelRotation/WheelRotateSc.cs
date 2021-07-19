5d936ab26cba35c34c31b32290a87d7f1d7aed74
DESKTOP-JM0Q4QR\Kzljd <onurhangumus@outlook.com>
2021-07-19 09:23:13 +0300

    wheels

---
+++ Assets/Scripts/WheelRotation/WheelRotateSc.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TopDownShooter.UserInput;
+
+namespace TopDownShooter.WheelRotate
+{
+    public class WheelRotateSc : MonoBehaviour
+    {
+        [SerializeField]
+        InputManagerSet inputManagerSet;
+        [SerializeField]
+        WheelRotateSet wheelRotateSet;
+        [SerializeField]
+        Transform oneOfFrontWheel = null;
+        
+        void Start()
+        {
+
+        }
+
+        void Update()
+        {
+
+            transform.Rotate(inputManagerSet.Vertical * wheelRotateSet.wheelRollingSpeed, 0/**speed*/, 0, Space.Self);
+            //transform.rotation = Quaternion.Euler(inputManagerSet.Vertical * wheelRotateSet.wheelRollingSpeed, 0, 0);
+            //transform.eulerAngles = new Vector3(transform.eulerAngles.x +inputManagerSet.Vertical * wheelRotateSet.wheelRollingSpeed, transform.rotation.y/**speed*/, 0);
+            if (oneOfFrontWheel != null)
+            {
+
+                //Debug.Log(("local euler: "+oneOfFrontWheel.localEulerAngles.y));
+                //Debug.Log(("normal euler: "+oneOfFrontWheel.eulerAngles.y));
+                //Debug.Log(("normal rotation: "+oneOfFrontWheel.rotation.y));
+                //Debug.Log(("local rotation: "+oneOfFrontWheel.localRotation.y));
+
+                //if ( (oneOfFrontWheel.localEulerAngles.y<36 || oneOfFrontWheel.localEulerAngles.y>320))
+                {
+                    //oneOfFrontWheel.Rotate(0, inputManagerSet.Horizontal * wheelRotateSet.wheelRotationSpeed/**speed*/, 0);
+                    oneOfFrontWheel.localEulerAngles = new Vector3(oneOfFrontWheel.localEulerAngles.x,0 + inputManagerSet.Horizontal * 16, oneOfFrontWheel.localEulerAngles.z);
+                }
+              
+
+            }
+
+        }
+    }
+
+}
+
+
---
+++ Assets/Scripts/WheelRotation/WheelRotateSet.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TopDownShooter.WheelRotate
+{
+    [CreateAssetMenu(menuName = "Lessons/Lessons1/Wheel Settings")]
+    public class WheelRotateSet : ScriptableObject
+    {
+        public float wheelRollingSpeed = 1;
+        public float wheelRotationSpeed = 1;
+
+    }
+}
